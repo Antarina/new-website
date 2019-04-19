dc1bcfe1fd13f8da4404579a8716c712cbd82311
Jelena Antic <antic.jelenaantic@gmail.com>
2019-04-19 21:43:13 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,15 +8,6 @@ function slideRight() {
     document.getElementById('mainContent').classList.add('non-active-right');
 }
 
-function slideDown() {
-    document.getElementById('bottomContent').classList.add('active-screen');
-    document.getElementById('mainContent').classList.add('non-active-bottom');
-    document.getElementById('home').classList.remove('active');
-    document.getElementById('biography').classList.add('active');
-    document.getElementById('rightContent').classList.remove('active-screen');
-    document.getElementById('leftContent').classList.remove('active-screen');
-}
-
 function slideBackLeft() {
     document.getElementById('rightContent').classList.remove('active-screen');
     document.getElementById('mainContent').classList.remove('non-active-right');
@@ -27,16 +18,5 @@ function slideBackRight() {
     document.getElementById('mainContent').classList.remove('non-active');
 }
 
-function slideToTop() {
-    document.getElementById('bottomContent').classList.remove('active-screen');
-    document.getElementById('mainContent').classList.remove('non-active-bottom');
-    document.getElementById('home').classList.add('active');
-    document.getElementById('biography').classList.remove('active');
-    document.getElementById('mainContent').classList.remove('non-active-right');
-    document.getElementById('mainContent').classList.remove('non-active');
-    document.getElementById('leftContent').classList.remove('active-screen');
-    document.getElementById('rightContent').classList.remove('active-screen');
-    const container = document.querySelector('.container')
-    container.scrollTop
-    container.scrollLeft
-}
+
+
